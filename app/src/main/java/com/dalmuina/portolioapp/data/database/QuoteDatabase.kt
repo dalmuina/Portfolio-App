7207aff2af9d13f3042cdf62018f24c3e7ecbaec
Diego <dalmuina@gmail.com>
2025-03-05 20:37:28 +0100

    Test workflow Database export scheme false

--- app/src/main/java/com/dalmuina/portolioapp/data/database/QuoteDatabase.kt
+++ app/src/main/java/com/dalmuina/portolioapp/data/database/QuoteDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.dalmuina.portolioapp.data.database.dao.QuoteDao
 import com.dalmuina.portolioapp.data.database.entities.QuoteEntity
 
-@Database(entities = [QuoteEntity::class], version=1)
+@Database(entities = [QuoteEntity::class], version=1, exportSchema = false)
 abstract class QuoteDatabase: RoomDatabase() {
     abstract fun getQuoteDao(): QuoteDao
 }
